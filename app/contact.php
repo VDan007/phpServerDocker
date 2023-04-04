26463a5b3a9a63ddd306249c1248cf00f1b8dbe5
DanielVarjaskéri <varjaskerid@gmail.com>
2023-04-04 22:15:47 +0200

    allways say php in the start

--- app/contact.php
+++ app/contact.php
@@ -1,3 +1,4 @@
 <?php
+require("./functions.php");
 $heading= "Contact";
 require("./views/view.contact.php");
---
+++ app/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+
+
+
+
+
+function urlIs($value){
+  
+    return $_SERVER['REQUEST_URI'] === $value;
+}
