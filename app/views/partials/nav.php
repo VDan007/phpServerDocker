73dd4733f95ce19dd168f90461c859ff8d5cfe0b
DanielVarjaskéri <varjaskerid@gmail.com>
2023-04-04 22:18:28 +0200

    all bugs fixed

--- app/views/partials/nav.php
+++ app/views/partials/nav.php
@@ -11,13 +11,13 @@
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
               <a href="/" 
-                class= "<?php echo urlIs('/') ?  "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" : "text-gray-300" ?>hover:bg-gray-700  rounded-md px-3 py-2 text-sm font-medium"  aria-current="page">Home</a>
+                class= "<?php echo urlIs('/') ?  "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" : "text-gray-300" ?>hover:bg-gray-700 text-white  rounded-md px-3 py-2 text-sm font-medium">Home</a>
 
               <a href="./about.php" 
-                 class="<?php echo urlIs('/about.php') ?  "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" : "text-gray-300" ?>hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
+                 class="<?php echo urlIs('/about.php') ?  "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" : "text-gray-300" ?>hover:bg-gray-700 text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
 
               <a href="/contact.php" 
-                 class="<?php echo urlIs('/contact.php') ?  "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" : "text-gray-300" ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
+                 class="<?php echo urlIs('/contact.php') ?  "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" : "text-gray-300" ?> hover:bg-gray-700 text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
 
             
             </div>
