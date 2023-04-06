51189254abfc0ceb202fe0f84143ea234bebb32e
DanielVarjaskéri <varjaskerid@gmail.com>
2023-04-06 17:52:20 +0200

    router works

--- index.php
+++ index.php
@@ -2,14 +2,19 @@
 
 require'./functions.php';
 
-$url =  $_SERVER['REQUEST_URI'];
+$uri =  parse_url($_SERVER['REQUEST_URI'])['path'];
 
 //dd( parse_url($url)['path'] );
 
-if($url === '/'){
-    require'./controllers/home.php';
-}else if($url ==='/about'){
-    require'./controllers/about.php';
-}else if($url === '/contact'){
-    require'./controllers/contact.php';
+$routes = [
+    '/' => 'controllers/home.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php'
+];
+
+if (array_key_exists($uri, $routes)){
+    require $routes[$uri];
 }
+
+
+
