87fd8e97b8b632d53af13acce1bc54ba5277ce06
DanielVarjaskéri <varjaskerid@gmail.com>
2023-04-06 21:56:24 +0200

    router fully ready

--- index.php
+++ index.php
@@ -1,20 +1,7 @@
 <?php
 
 require'./functions.php';
-
-$uri =  parse_url($_SERVER['REQUEST_URI'])['path'];
-
-//dd( parse_url($url)['path'] );
-
-$routes = [
-    '/' => 'controllers/home.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php'
-];
-
-if (array_key_exists($uri, $routes)){
-    require $routes[$uri];
-}
+require './router.php';
 
 
 
