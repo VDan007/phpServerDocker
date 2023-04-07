baebf87eb642d88c085ef279e3c09d6c36db4048
DanielVarjaskéri <varjaskerid@gmail.com>
2023-04-08 00:59:39 +0200

    class learned

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
 
 require'./functions.php';
-require './router.php';
+//require './router.php';
 
 
 
+class Person {
+    public $name;
+    public $age;
+    public function breathe(){
+        echo $this->name . ' is breathing';
+    }
+}
+
+$person = new Person ();
+
+$person ->name = 'Jhon Doe';
+$person ->age = 25;
+
+dd($person->breathe());
