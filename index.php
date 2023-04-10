b21b3aec1cb3eb8aa0d35d2bee5e8a3d639e4233
DanielVarjaskéri <varjaskerid@gmail.com>
2023-04-10 13:19:43 +0200

    routing I have to solve

--- index.php
+++ index.php
@@ -9,12 +9,12 @@ require'./functions.php';
 
 
 
-$dsn= "mysql:host=172.19.0.2;port=3306;dbname=myapp;charset=utf8";
+$dsn= "mysql:host=172.27.0.2;port=3306;dbname=myapp;charset=utf8";
 $pdo = new PDO($dsn ,'root','super-secret-password');
 
 $statement = $pdo->prepare("SELECT * from posts");
 $statement->execute();
 
-$post = $statement->fetchAll();
+$post = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 dd($post);
